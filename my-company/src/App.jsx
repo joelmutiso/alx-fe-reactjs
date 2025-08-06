4ead883b9f542ecb457151035eded175c63c4331
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-06 20:45:36 +0300

    updated components with Navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Components/Home';
-import About from './Components/About';
-import Services from './Components/Services';
-import Contact from './Components/Contact';
-import Navbar from './Components/Navbar'
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
 
 function Footer() {
   return (
