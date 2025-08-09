59da09fa07cd563ce7405d87c68f169c14869da3
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-10 00:53:48 +0300

    setup React project with Tailwind CSS using Vite

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./App.css";
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App;
