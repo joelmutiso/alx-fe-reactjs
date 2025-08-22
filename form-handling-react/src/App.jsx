656ecd8491de2eee07bea0a67f9e6d60ef1b6a4e
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-22 23:20:13 +0300

    Add user registration form

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import RegistrationForm from './components/RegistrationForm';
+import formikForm from './components/formikForm';
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen py-10">
       <RegistrationForm />
+      <formikForm />
     </div>
   );
 }
